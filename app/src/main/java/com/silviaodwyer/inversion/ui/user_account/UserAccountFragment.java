99a610af874e43f03a59f5e28d9000babe16a780
Silvia O'Dwyer <silviaodwyer119@yahoo.ie>
2020-04-30 17:30:42 +0100

    Added toggle switch and corresponding onChange listener to UserAccount activity.

--- app/src/main/java/com/silviaodwyer/inversion/ui/user_account/UserAccountFragment.java
+++ app/src/main/java/com/silviaodwyer/inversion/ui/user_account/UserAccountFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -11,11 +13,20 @@ import androidx.fragment.app.Fragment;
 import com.silviaodwyer.inversion.R;
 
 public class UserAccountFragment extends Fragment {
+  private Switch themeToggle;
 
   public View onCreateView(@NonNull LayoutInflater inflater,
                            ViewGroup container, Bundle savedInstanceState) {
 
     View root = inflater.inflate(R.layout.fragment_useraccount, container, false);
+
+    themeToggle = root.findViewById(R.id.themeToggle);
+    themeToggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+      @Override
+      public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+        // TODO switch theme
+      }
+    });
 
     return root;
   }

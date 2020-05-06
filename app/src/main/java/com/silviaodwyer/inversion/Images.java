528c688bc5ca8bfab453c77cc3ed7b7be12e0a61
Silvia O'Dwyer <silviaodwyer119@yahoo.ie>
2020-05-06 16:03:29 +0100

    Added image grid w/ GridView and corresponding ImagesAdapter to display images in Images activity.

--- app/src/main/java/com/silviaodwyer/inversion/Images.java
+++ app/src/main/java/com/silviaodwyer/inversion/Images.java
@@ -3,6 +3,7 @@ package com.silviaodwyer.inversion;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.GridView;
 
 public class Images extends AppCompatActivity {
 
@@ -10,5 +11,13 @@ public class Images extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_images);
+
+    initGridView();
+  }
+
+  public void initGridView() {
+    GridView gridView = (GridView) findViewById(R.id.images_grid_view);
+    ImagesAdapter imagesAdapter = new ImagesAdapter(this);
+    gridView.setAdapter(imagesAdapter);
   }
 }
---
+++ app/src/main/java/com/silviaodwyer/inversion/ImagesAdapter.java
@@ -0,0 +1,50 @@
+package com.silviaodwyer.inversion;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class ImagesAdapter extends BaseAdapter {
+
+  private final Context mContext;
+  private final ArrayList<String> images = new ArrayList<String>();
+
+  public ImagesAdapter(Context context) {
+    this.mContext = context;
+    for (int i = 0; i < 30; i++) {
+      String img_name = "Image" + i;
+      images.add(img_name);
+    }
+  }
+
+  @Override
+  public int getCount() {
+    return images.size();
+  }
+
+  @Override
+  public Object getItem(int position) {
+    return null;
+  }
+
+  @Override
+  public long getItemId(int position) {
+    return 0;
+  }
+
+  @Override
+  public View getView(int position, View convertView, ViewGroup parent) {
+    ImageView imageView = new ImageView(mContext);
+    imageView.setImageResource(R.drawable.gradient);
+    imageView.setMinimumWidth(30);
+    imageView.setMinimumHeight(90);
+    return imageView;
+  }
+
+}

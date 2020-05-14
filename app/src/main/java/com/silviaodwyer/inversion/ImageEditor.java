5afce1af0071062f5dd068a22b40019c1bf09ea3
Silvia O'Dwyer <silviaodwyer119@yahoo.ie>
2020-05-14 18:49:21 +0100

    Integrated GPUImage-Android library into the project.

--- app/src/main/java/com/silviaodwyer/inversion/ImageEditor.java
+++ app/src/main/java/com/silviaodwyer/inversion/ImageEditor.java
@@ -1,18 +1,44 @@
 package com.silviaodwyer.inversion;
 
+import android.graphics.Bitmap;
+import android.graphics.Matrix;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.NavigationUI;
+import jp.co.cyberagent.android.gpuimage.GPUImage;
+import jp.co.cyberagent.android.gpuimage.filter.GPUImageAddBlendFilter;
+import jp.co.cyberagent.android.gpuimage.filter.GPUImageBulgeDistortionFilter;
+import jp.co.cyberagent.android.gpuimage.filter.GPUImageColorInvertFilter;
+import jp.co.cyberagent.android.gpuimage.filter.GPUImageContrastFilter;
+import jp.co.cyberagent.android.gpuimage.filter.GPUImageExclusionBlendFilter;
+import jp.co.cyberagent.android.gpuimage.filter.GPUImageFilter;
+import jp.co.cyberagent.android.gpuimage.filter.GPUImageHueBlendFilter;
+import jp.co.cyberagent.android.gpuimage.filter.GPUImageLaplacianFilter;
+import jp.co.cyberagent.android.gpuimage.filter.GPUImagePosterizeFilter;
+import jp.co.cyberagent.android.gpuimage.filter.GPUImageSepiaToneFilter;
+import jp.co.cyberagent.android.gpuimage.filter.GPUImageSourceOverBlendFilter;
+import jp.co.cyberagent.android.gpuimage.filter.GPUImageVignetteFilter;
 
 public class ImageEditor extends AppCompatActivity {
   ImageView imageView;
+  Bitmap bmp;
+  Bitmap originalImageBitmap;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +54,30 @@ public class ImageEditor extends AppCompatActivity {
     // set image
     MainApplication application = ((MainApplication)getApplication());
     Uri imageUri = application.getImageUri();
-    imageView.setImageURI(imageUri);
+
+    try {
+      updateGPUImageView(imageUri);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  private void updateGPUImageView(Uri imageUri) throws IOException {
+    saveBitmaps(imageUri);
+    GPUImage gpuImage = new GPUImage(getApplicationContext());
+
+    gpuImage.setImage(bmp);
+    bmp = gpuImage.getBitmapWithFilterApplied(bmp);
+
+    // Set the ImageView's image content to the current value of bmp
+    imageView.setImageBitmap(bmp);
+
+    final GPUImage mGPUImage = new GPUImage(this);
+  }
+
+  private void saveBitmaps(Uri imageUri) throws IOException {
+    bmp = MediaStore.Images.Media.getBitmap(this.getContentResolver(), imageUri);
+    originalImageBitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), imageUri);
   }
 
 }

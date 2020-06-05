db1eb7a7d43f1f91f93be3a714335eb62173dc41
Silvia O'Dwyer <silviaodwyer119@yahoo.ie>
2020-06-05 18:14:39 +0100

    Added JavaDoc comments to MainApplication class.

--- app/src/main/java/com/silviaodwyer/inversion/MainApplication.java
+++ app/src/main/java/com/silviaodwyer/inversion/MainApplication.java
@@ -20,15 +20,30 @@ public class MainApplication extends Application {
   private ArrayList<String> imageNames = new ArrayList<String>();
   private static String savedImagePathFilename = "saved_image_paths.json";
   private static String IMAGES_DIRECTORY = "imagesDirectory";
+  private static String IMAGE_EFFECTS_LIST = "image_effects_list.json";
 
+  /**
+   * Set the active video URL
+   *
+   */
   public void setVideoUrl(String vidUrl) {
     this.videoUrl = vidUrl;
   }
 
+  /**
+   * Returns the active video URL
+   *
+   * @return      active video URL
+   */
   public String getVideoUrl() {
     return videoUrl;
   }
 
+  /**
+   * Returns an instance of the ImageEditor activity
+   *
+   * @return      instance of the ImageEditor activity
+   */
   public ImageEditor getImageEditorActivity() {
     return imageEditorActivity;
   }
@@ -79,10 +94,6 @@ public class MainApplication extends Application {
     return savedImagePaths;
   }
 
-  public void setImageNames(ArrayList<String> imageNames) {
-    this.imageNames = imageNames;
-  }
-
   /**
    * Returns the name of the file which contains the names of all images stored by the user
    *
@@ -92,9 +103,27 @@ public class MainApplication extends Application {
     return savedImagePathFilename;
   }
 
+  /**
+   * Add a new image name (for an image that was newly saved and was not
+   * previously in the list) to the list of saved image names
+   *
+   * @param image_name
+   * @param context
+   */
   public void addSavedImageName(String image_name, Context context) {
     imageNames = getSavedImageNames(context);
     imageNames.add(image_name);
+  }
+
+  /**
+   * Returns the name of the image effects list file.
+   * The image effects list file is a JSON file containing a list of
+   * all available image effects and filters, plus descriptions of each.
+   *
+   * @return      image instance
+   */
+  public static String getImageEffectsList() {
+    return IMAGE_EFFECTS_LIST;
   }
 
 }

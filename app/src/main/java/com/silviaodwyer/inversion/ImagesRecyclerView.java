0df1f7e447862032f909c512c24c57385c0a5066
Silvia O'Dwyer <silviaodwyer119@yahoo.ie>
2020-06-08 18:07:18 +0100

    Added ImagesRecyclerView and corresponding RecyclerView layout item.

---
+++ app/src/main/java/com/silviaodwyer/inversion/ImagesRecyclerView.java
@@ -0,0 +1,85 @@
+package com.silviaodwyer.inversion;
+
+import android.content.Context;
+import android.content.ContextWrapper;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class ImagesRecyclerView extends RecyclerView.Adapter<ImagesRecyclerView.ViewHolder> {
+
+  private ArrayList<Bitmap> data;
+  private ItemClickListener clickListener;
+  private LayoutInflater inflater;
+  private Context context;
+  private MainApplication mainApplication;
+
+  ImagesRecyclerView(Context context, ArrayList<Bitmap> data, MainApplication mainApplication) {
+    this.inflater = LayoutInflater.from(context);
+    this.data = data;
+    this.context = context;
+    this.mainApplication = mainApplication;
+  }
+
+  @Override
+  @NonNull
+  public ImagesRecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    View view = inflater.inflate(R.layout.recyclerview_image, parent, false);
+    return new ViewHolder(view);
+  }
+
+  // binds the bitmap to the ImageView
+  @Override
+  public void onBindViewHolder(@NonNull ImagesRecyclerView.ViewHolder holder, int position) {
+    holder.imageView.setImageBitmap(data.get(position));
+  }
+
+  @Override
+  public int getItemCount() {
+    return data.size();
+  }
+
+  // Recycles views when they are being scrolled off the screen
+  public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+    ImageView imageView;
+
+    ViewHolder(View itemView) {
+      super(itemView);
+      imageView = itemView.findViewById(R.id.image_thumbnail);
+      itemView.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+      if (clickListener != null) {
+        clickListener.onItemClick(view, getAdapterPosition());
+        Intent intent = new Intent(context, ImageEditor.class);
+        Image image = new Image(data.get(getAdapterPosition()), context, mainApplication.getImageEditorActivity());
+        mainApplication.setImage(image);
+        context.startActivity(intent);
+      }
+    }
+
+  }
+
+  Bitmap getItem(int id) {
+    return data.get(id);
+  }
+
+  void setClickListener(ItemClickListener itemClickListener) {
+    this.clickListener = itemClickListener;
+  }
+
+  public interface ItemClickListener {
+    void onItemClick(View view, int position);
+  }
+}

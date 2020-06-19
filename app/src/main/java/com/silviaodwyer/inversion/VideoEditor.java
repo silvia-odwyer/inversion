f68e6468d70eadcea6dc270a0223cc689be5036d
Silvia O'Dwyer <silviaodwyer119@yahoo.ie>
2020-06-19 17:41:38 +0100

    Added BitmapAsyncTask to asynchronously load the thumbnail bitmap from the video uploaded.

--- app/src/main/java/com/silviaodwyer/inversion/VideoEditor.java
+++ app/src/main/java/com/silviaodwyer/inversion/VideoEditor.java
@@ -5,8 +5,12 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.media.ThumbnailUtils;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
@@ -15,9 +19,16 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.RequestBuilder;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.request.FutureTarget;
+import com.bumptech.glide.request.RequestListener;
 import com.bumptech.glide.request.RequestOptions;
+import com.bumptech.glide.request.target.SimpleTarget;
+import com.bumptech.glide.request.target.Target;
 import com.daasuu.epf.EPlayerView;
 import com.daasuu.epf.filter.GlFilter;
 import com.daasuu.epf.filter.GlSepiaFilter;
@@ -33,17 +44,22 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.concurrent.ExecutionException;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import static com.bumptech.glide.load.resource.UnitTransformation.get;
+
 public class VideoEditor extends AppCompatActivity {
   private MainApplication mainApplication;
   private String videoPath;
   private EPlayerView ePlayerView;
+  private ImageView imageView;
   private String videoURL = "https://www.radiantmediaplayer.com/media/bbb-360p.mp4";
   private String imageURL = "https://images.unsplash.com/photo-1567359781514-3b964e2b04d6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDkwNH0&fm=png&w=100";
 
@@ -63,6 +79,8 @@ public class VideoEditor extends AppCompatActivity {
     mainApplication = ((MainApplication)getApplication());
 
     mainApplication.requestPermissions(VideoEditor.this);
+
+
     setupVideo();
   }
 
@@ -86,6 +104,14 @@ public class VideoEditor extends AppCompatActivity {
     linLayout.addView(ePlayerView);
     ePlayerView.onResume();
     filterPlayerView(new GlSepiaFilter());
+
+    try {
+      thumbnailVideo(videoURL);
+    } catch (ExecutionException e) {
+      e.printStackTrace();
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
   }
 
   @Override
@@ -129,7 +155,54 @@ public class VideoEditor extends AppCompatActivity {
     }
   }
 
+  public void thumbnailVideo(final String videoPath) throws ExecutionException, InterruptedException {
+    new Thread(new Runnable() {
+      @Override
+      public void run() {
+        new BitmapAsyncTask().execute();
+      }
+    }).start();
+
+  }
+
+
+  class BitmapAsyncTask extends AsyncTask<Void, Integer, Bitmap> {
+
+    protected void onPreExecute() {
+      super.onPreExecute();
+    }
+
+    protected Bitmap doInBackground(Void...arg0) {
+      Bitmap bitmap = null;
+      FutureTarget<Bitmap> futureTarget =
+        Glide.with(getApplicationContext())
+          .asBitmap()
+          .load(videoPath)
+          .submit(50, 50);
+
+      try {
+        bitmap = futureTarget.get();
+      } catch (ExecutionException e) {
+        e.printStackTrace();
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+      return bitmap;
+    }
+
+    protected void onProgressUpdate(Integer...a) {
+      super.onProgressUpdate(a);
+    }
+
+    protected void onPostExecute(Bitmap result) {
+      ImageView imageView2 = findViewById(R.id.vidthumbnail);
+      imageView2.setImageBitmap(result);
+      super.onPostExecute(result);
+    }
+  }
+
   private void filterPlayerView(GlFilter filter) {
     ePlayerView.setGlFilter(filter);
   }
+
 }

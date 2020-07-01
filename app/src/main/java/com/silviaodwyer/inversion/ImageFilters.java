fe24c1ebb23eb28fca025c5a5bc53048a2496ad2
Silvia O'Dwyer <silviaodwyer119@yahoo.ie>
2020-07-01 18:39:11 +0100

    Added contrast and saturation SeekBars w/ listeners to the image correction menu fragment.

--- app/src/main/java/com/silviaodwyer/inversion/ImageFilters.java
+++ app/src/main/java/com/silviaodwyer/inversion/ImageFilters.java
@@ -2,6 +2,8 @@ package com.silviaodwyer.inversion;
 
 import android.graphics.Bitmap;
 import android.graphics.Matrix;
+import android.graphics.PointF;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -44,6 +46,11 @@ public class ImageFilters {
     filters.add(getFourFilter());
     filters.add(getEightFilter());
     filters.add(getRubrikFilter());
+    filters.add(getCaliFilter());
+    filters.add(getRetroFilter());
+    filters.add(getRetroFilter2());
+    filters.add(getRetroFilter3());
+    filters.add(getRetroFilter4());
   }
 
   public GPUImageFilterGroup getDramaticFilter() {
@@ -103,6 +110,167 @@ public class ImageFilters {
     GPUImageFilterGroup filterGroup = new GPUImageFilterGroup();
     filterGroup.addFilter(new GPUImageContrastFilter(amount));
     filterGroup.addFilter(new GPUImageVibranceFilter());
+    return filterGroup;
+  }
+
+  public GPUImageFilterGroup getCaliFilter() {
+    float amt = (float) 1.1;
+    float amt2 = (float) 1.2;
+    float r_amt = (float) 0.1;
+    float g_amt = (float) 0.0;
+    float b_amt = (float) 0.0;
+    GPUImageFilterGroup filterGroup = new GPUImageFilterGroup();
+    filterGroup.addFilter(new GPUImageSaturationFilter(amt));
+    filterGroup.addFilter(new GPUImageRGBFilter(r_amt, g_amt, b_amt));
+    filterGroup.addFilter(new GPUImageContrastFilter(amt2));
+
+    return filterGroup;
+  }
+
+  public GPUImageFilterGroup getRetroFilter() {
+    GPUImageFilterGroup filterGroup = new GPUImageFilterGroup();
+
+//    GPUImageAddBlendFilter blendFilter = new GPUImageAddBlendFilter();
+//    blendFilter.setBitmap(BitmapFactory.decodeResource(
+//      getApplicationContext().getResources(), R.mipmap.background));
+//    filterGroup.addFilter(blendFilter);
+
+    GPUImageToneCurveFilter toneCurve = new GPUImageToneCurveFilter();
+    PointF[] rControlPoints = new PointF[] { new PointF(0.0f, 0.145f),
+            new PointF(0.063f, 0.153f), new PointF(0.251f, 0.278f),
+            new PointF(0.573f, 0.776f), new PointF(0.624f, 0.863f),
+            new PointF(0.682f, 0.922f), new PointF(0.792f, 0.965f),
+            new PointF(1.0f, 1.0f) };
+
+    PointF[] gControlPoints = new PointF[] { new PointF(0.0f, 0.0f),
+            new PointF(0.255f, 0.196f), new PointF(0.447f, 0.576f),
+            new PointF(0.686f, 0.875f), new PointF(1.0f, 1.0f) };
+
+    PointF[] bControlPoints = new PointF[] { new PointF(0.0f, 0.137f),
+            new PointF(0.251f, 0.251f), new PointF(0.345f, 0.376f),
+            new PointF(0.608f, 0.698f), new PointF(0.890f, 0.91f),
+            new PointF(1.0f, 0.941f) };
+
+    toneCurve.setRedControlPoints(rControlPoints);
+    toneCurve.setGreenControlPoints(gControlPoints);
+    toneCurve.setBlueControlPoints(bControlPoints);
+
+    filterGroup.addFilter(toneCurve);
+
+    GPUImageSaturationFilter saturation = new GPUImageSaturationFilter();
+    saturation.setSaturation(0.8f);
+    filterGroup.addFilter(saturation);
+
+    return filterGroup;
+  }
+
+  public GPUImageFilterGroup getRetroFilter2() {
+    GPUImageFilterGroup filterGroup = new GPUImageFilterGroup();
+
+    GPUImageToneCurveFilter toneCurve = new GPUImageToneCurveFilter();
+    PointF[] rControlPoints = new PointF[] { new PointF(0.0f, 0.145f),
+            new PointF(0.063f, 0.153f), new PointF(0.251f, 0.278f),
+            new PointF(0.573f, 0.776f), new PointF(0.624f, 0.863f),
+            new PointF(0.682f, 0.922f), new PointF(0.792f, 0.965f),
+            new PointF(1.0f, 1.0f) };
+
+    PointF[] gControlPoints = new PointF[] { new PointF(0.0f, 0.0f),
+            new PointF(0.255f, 0.196f), new PointF(0.447f, 0.576f),
+            new PointF(0.686f, 0.875f), new PointF(1.0f, 1.0f) };
+
+    PointF[] bControlPoints = new PointF[] { new PointF(0.0f, 0.137f),
+            new PointF(0.251f, 0.251f), new PointF(0.345f, 0.376f),
+            new PointF(0.608f, 0.698f), new PointF(0.890f, 0.91f),
+            new PointF(1.0f, 0.941f) };
+
+    toneCurve.setRedControlPoints(rControlPoints);
+    toneCurve.setGreenControlPoints(gControlPoints);
+    toneCurve.setBlueControlPoints(bControlPoints);
+
+    filterGroup.addFilter(toneCurve);
+
+    GPUImageSaturationFilter saturation = new GPUImageSaturationFilter();
+    saturation.setSaturation(0.8f);
+    filterGroup.addFilter(saturation);
+
+    GPUImageContrastFilter contrast = new GPUImageContrastFilter();
+    contrast.setContrast(0.8f);
+    filterGroup.addFilter(contrast);
+
+    return filterGroup;
+  }
+
+
+  public GPUImageFilterGroup getRetroFilter3() {
+    GPUImageFilterGroup filterGroup = new GPUImageFilterGroup();
+    float amt = (float) 0.8;
+
+    GPUImageGrayscaleFilter filter = new GPUImageGrayscaleFilter();
+    filterGroup.addFilter(filter);
+
+    GPUImageToneCurveFilter toneCurve = new GPUImageToneCurveFilter();
+    PointF[] rControlPoints = new PointF[] { new PointF(0.0f, 0.145f),
+            new PointF(0.063f, 0.153f), new PointF(0.251f, 0.278f),
+            new PointF(0.573f, 0.776f), new PointF(0.624f, 0.863f),
+            new PointF(0.682f, 0.922f), new PointF(0.792f, 0.965f),
+            new PointF(1.0f, 1.0f) };
+
+    PointF[] gControlPoints = new PointF[] { new PointF(0.0f, 0.0f),
+            new PointF(0.255f, 0.196f), new PointF(0.447f, 0.576f),
+            new PointF(0.686f, 0.875f), new PointF(1.0f, 1.0f) };
+
+    PointF[] bControlPoints = new PointF[] { new PointF(0.0f, 0.137f),
+            new PointF(0.251f, 0.251f), new PointF(0.345f, 0.376f),
+            new PointF(0.608f, 0.698f), new PointF(0.890f, 0.91f),
+            new PointF(1.0f, 0.941f) };
+
+    toneCurve.setRedControlPoints(rControlPoints);
+    toneCurve.setGreenControlPoints(gControlPoints);
+    toneCurve.setBlueControlPoints(bControlPoints);
+
+    filterGroup.addFilter(toneCurve);
+
+    GPUImageSaturationFilter saturation = new GPUImageSaturationFilter();
+    saturation.setSaturation(0.8f);
+    filterGroup.addFilter(saturation);
+
+    return filterGroup;
+  }
+
+
+  public GPUImageFilterGroup getRetroFilter4() {
+    GPUImageFilterGroup filterGroup = new GPUImageFilterGroup();
+    float amt = (float) 0.8;
+
+    GPUImageGrayscaleFilter filter = new GPUImageGrayscaleFilter();
+    filterGroup.addFilter(filter);
+
+    GPUImageToneCurveFilter toneCurve = new GPUImageToneCurveFilter();
+    PointF[] rControlPoints = new PointF[] { new PointF(0.0f, 0.145f),
+            new PointF(0.063f, 0.153f), new PointF(0.251f, 0.278f),
+            new PointF(0.573f, 0.79f), new PointF(0.624f, 0.863f),
+            new PointF(0.682f, 0.22f), new PointF(0.792f, 0.965f),
+            new PointF(1.0f, 1.0f) };
+
+    PointF[] gControlPoints = new PointF[] { new PointF(0.0f, 0.0f),
+            new PointF(0.255f, 0.196f), new PointF(0.447f, 0.576f),
+            new PointF(0.686f, 0.875f), new PointF(1.0f, 1.0f) };
+
+    PointF[] bControlPoints = new PointF[] { new PointF(0.0f, 0.137f),
+            new PointF(0.51f, 0.51f), new PointF(0.15f, 0.376f),
+            new PointF(0.08f, 0.98f), new PointF(0.190f, 0.91f),
+            new PointF(1.0f, 0.941f) };
+
+    toneCurve.setRedControlPoints(rControlPoints);
+    toneCurve.setGreenControlPoints(gControlPoints);
+    toneCurve.setBlueControlPoints(bControlPoints);
+
+    filterGroup.addFilter(toneCurve);
+
+    GPUImageSaturationFilter saturation = new GPUImageSaturationFilter();
+    saturation.setSaturation(0.8f);
+    filterGroup.addFilter(saturation);
+
     return filterGroup;
   }
 
@@ -166,5 +334,21 @@ public class ImageFilters {
   public void filterImage(GPUImageFilter filter, Image image) {
     ImageEditor activity = image.getActivity();
     activity.updateGPUImage(filter);
+  }
+
+  public void brightenImage(Image image, float amt) {
+    GPUImageFilter brightnessFilter = new GPUImageBrightnessFilter(amt);
+    filterImage(brightnessFilter, image);
+  }
+
+  public void adjustSaturation(Image image, float amt) {
+    GPUImageFilter saturationFilter = new GPUImageSaturationFilter(amt);
+    filterImage(saturationFilter, image);
+  }
+
+  public void adjustContrast(Image image, float amt) {
+    Log.d("DEBUG", "ADJUST CONTRAST");
+    GPUImageFilter contrastFilter = new GPUImageContrastFilter((float) 0.9);
+    filterImage(contrastFilter, image);
   }
 }
--- app/src/main/java/com/silviaodwyer/inversion/image_editor_menus/correct_menu/CorrectMenuFragment.java
+++ app/src/main/java/com/silviaodwyer/inversion/image_editor_menus/correct_menu/CorrectMenuFragment.java
@@ -6,6 +6,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -13,6 +15,7 @@ import jp.co.cyberagent.android.gpuimage.filter.GPUImageFilter;
 
 import com.silviaodwyer.inversion.Image;
 import com.silviaodwyer.inversion.ImageEditor;
+import com.silviaodwyer.inversion.ImageFile;
 import com.silviaodwyer.inversion.ImageFilters;
 import com.silviaodwyer.inversion.MainApplication;
 import com.silviaodwyer.inversion.R;
@@ -23,6 +26,8 @@ public class CorrectMenuFragment extends Fragment {
 
   private ImageEditor activity;
   private Image image;
+  private ImageFilters imageFilters;
+  private TextView brightnessTextVal;
 
   public View onCreateView(@NonNull LayoutInflater inflater,
                            ViewGroup container, Bundle savedInstanceState) {
@@ -32,12 +37,52 @@ public class CorrectMenuFragment extends Fragment {
 
     image = new Image(mainApplication.getImage().getBitmap(), root.getContext(), activity, mainApplication.getImage().getMetaData());
 
-    ImageFilters imageFilters = new ImageFilters();
+    imageFilters = new ImageFilters();
 
-    LinearLayout filteredImagesLinLayout = root.findViewById(R.id.correctedImageThumbnails);
-    ArrayList<Bitmap> thumbnails = imageFilters.generateThumbnails(image, ImageFilters.FilterType.CORRECTION);
-    imageFilters.appendImageThumbnails(filteredImagesLinLayout, image, thumbnails);
+//    LinearLayout filteredImagesLinLayout = root.findViewById(R.id.correctedImageThumbnails);
+//    ArrayList<Bitmap> thumbnails = imageFilters.generateThumbnails(image, ImageFilters.FilterType.CORRECTION);
+//    imageFilters.appendImageThumbnails(filteredImagesLinLayout, image, thumbnails);
+
+    // set a change listener on the SeekBar
+    SeekBar brightnessSlider = root.findViewById(R.id.brightness);
+    brightnessSlider.setOnSeekBarChangeListener(sliderChangeListener);
+
+    SeekBar contrastSlider = root.findViewById(R.id.contrast);
+    brightnessSlider.setOnSeekBarChangeListener(sliderChangeListener);
+
+    SeekBar saturationSlider = root.findViewById(R.id.saturation);
+    saturationSlider.setOnSeekBarChangeListener(sliderChangeListener);
 
     return root;
   }
+
+  SeekBar.OnSeekBarChangeListener sliderChangeListener = new SeekBar.OnSeekBarChangeListener() {
+
+    @Override
+    public void onProgressChanged(SeekBar slider, int progress, boolean fromUser) {
+      String sliderTag = slider.getTag().toString();
+
+      switch (sliderTag) {
+        case "brightness":
+          imageFilters.brightenImage(image, (float) (progress / 100.0));
+          break;
+        case "contrast":
+          imageFilters.adjustContrast(image, (float) (progress / 100.0));
+          break;
+        case "saturation":
+          imageFilters.adjustSaturation(image, (float) (progress / 100.0));
+          break;
+      }
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+
+    }
+  };
 }
